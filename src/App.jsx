a920322361b6c035e0f99da1cd62dd9e3ce77d20
hanjongseop <whd4355@gmail.com>
2024-05-20 15:52:09 +0900

    제출

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+
 export default function App() {
+  const [count, SetCount] = useState(0);
+  const [plus, SetPlus] = useState("");
+  const [minus, SetMinus] = useState("");
+
+  const addPluse = () => {
+    SetPlus(Number + input);
+  };
+
+  const reset = () => {
+    SetCount(0);
+  };
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input
+          type="number"
+          value={count}
+          onChange={(e) => {
+            SetCount();
+          }}
+        />{" "}
+        만큼을
+        <button onClick={() => SetPlus}>더할게요</button>
+        <button>뺄게요</button>
+        <button onClick={() => SetCount}>초기화</button>
       </div>
       <hr />
       <div>
